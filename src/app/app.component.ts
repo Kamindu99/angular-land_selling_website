688142980a8b4ea477f2d2d1454aa5dc26c7c493
kamindu.g <kamindu.g@informaticsint.com>
2023-07-26 00:44:18 +0530

    set up angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'land_selling_websites-angular';
+  myname="Kamindu Gayantha";
+}
